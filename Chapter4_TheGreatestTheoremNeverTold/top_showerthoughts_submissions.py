1c2a4b666dc24bda09e01797cd72ebc4bacac121
gjcooper <gjcooper@gmail.com>
2019-05-17 11:04:01 +1000

    Working through chapters, addition of styling code

--- Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
+++ Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
@@ -4,19 +4,21 @@ import numpy as np
 from IPython.core.display import Image
 
 import praw
+print(praw.__version__)
 
 
-reddit = praw.Reddit("BayesianMethodsForHackers")
-subreddit  = reddit.get_subreddit("showerthoughts")
+reddit = praw.Reddit("bayesbot")
 
-top_submissions = subreddit.get_top(limit=100)
+subreddit = reddit.subreddit("showerthoughts")
 
-n_sub = int( sys.argv[1] ) if sys.argv[1] else 1
+top_submissions = subreddit.hot(limit=100)
+
+n_sub = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 
 i = 0
 while i < n_sub:
     top_submission = next(top_submissions)
-    i+=1
+    i += 1
 
 top_post = top_submission.title
 
@@ -26,11 +28,12 @@ contents = []
 
 for sub in top_submissions:
     try:
-        ratio = reddit.get_submission(sub.permalink).upvote_ratio
+        ratio = sub.upvote_ratio
         ups = int(round((ratio*sub.score)/(2*ratio - 1)) if ratio != 0.5 else round(sub.score/2))
         upvotes.append(ups)
         downvotes.append(ups - sub.score)
         contents.append(sub.title)
     except Exception as e:
         continue
+
 votes = np.array( [ upvotes, downvotes] ).T
